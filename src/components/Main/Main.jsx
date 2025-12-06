c2f74ad772a51f18240185c4201d08f4ab0d214b
Valeria Naranjo <kaiserinloli666@gmail.com>
2025-12-06 11:38:24 -0600

    update: main solved

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,6 @@
 import { Children } from "react";
 import "./Main.css";
-import Traveler from "./components/Traveler/Traveler.jsx";
 import Profile from "./components/Profile/Profile.jsx";
-import Card from "./components/Card/Card.jsx";
 import CardContainer from "./components/CardConteiner/CardConteriner.jsx"
 
 const Main = (props)  => {
